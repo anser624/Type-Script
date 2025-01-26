32f4da0923731651215e0d5681b6b2e59c5debec
anser624 <am24626526jan@gmail.com>
2025-01-27 00:42:18 +0500

    Update File-For-Class-2

---
+++ class-2/app.js
@@ -0,0 +1,14 @@
+// alias type 
+var person = {
+    name: "ansar",
+    age: 45,
+    id: true,
+    city: "karachi",
+};
+var person2 = {
+    name: "ali",
+    age: 30,
+    id: false,
+};
+console.log(person);
+console.log(person2);
